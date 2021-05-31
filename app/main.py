02edaf5a95f76fc8a110b91b3bfac695bc5a3aa9
iam-mhaseeb <haseeb.emailbox@gmail.com>
2021-06-01 00:30:58 +0500

    added test msgs

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ import os
 
 from slack_bolt import App
 
+from wsgi import app
+
 
 def get_slack_bolt_app():
     """
@@ -12,3 +14,8 @@ def get_slack_bolt_app():
         token=os.environ.get("SLACK_BOT_TOKEN"),
         signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
     )
+
+
+@app.message("hello")
+def message_hello(message, say):
+    say(f"Hey there <@{message['user']}>!")
