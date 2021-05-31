8ab9a50d93d32486f3651b1dd8bec3bddb75d05a
iam-mhaseeb <haseeb.emailbox@gmail.com>
2021-05-31 22:59:09 +0500

    Added .env file + added bot id generation method

--- app/main.py
+++ app/main.py
@@ -5,4 +5,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Welcome to CodingX</h1>"
+    return "<h1>Welcome to StandupMonkey</h1><br><p>A self hosted slack bot to conduct standups &amp; generate reports.</p>"
+
