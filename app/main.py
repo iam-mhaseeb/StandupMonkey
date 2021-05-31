5162a1021dc4c84b61fd600c6d5cf992deb5552e
iam-mhaseeb <haseeb.emailbox@gmail.com>
2021-06-01 00:35:45 +0500

    Fixed circular imports

--- app/main.py
+++ app/main.py
@@ -2,18 +2,10 @@ import os
 
 from slack_bolt import App
 
-from wsgi import app
-
-
-def get_slack_bolt_app():
-    """
-    Returns slack bolt app object after initializing.
-    :return: Slack bolt app object
-    """
-    return App(
-        token=os.environ.get("SLACK_BOT_TOKEN"),
-        signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
-    )
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
 
 
 @app.message("hello")
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,6 @@
 import os
 
-from app.main import get_slack_bolt_app
-
-app = get_slack_bolt_app()
+from app.main import app
 
 if __name__ == "__main__":
     app.start(port=int(os.environ.get("PORT", 3000)))
