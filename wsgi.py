a3cb507e34c2898b5a8980ce67b7d145a9c54bd4
iam-mhaseeb <haseeb.emailbox@gmail.com>
2021-06-01 00:25:36 +0500

    Added port from environment variable in wsgi file

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
+import os
+
 from app.main import get_slack_bolt_app
 
 app = get_slack_bolt_app()
 
 if __name__ == "__main__":
-    app.start(port=int(3000))
+    app.start(port=int(os.environ.get("PORT", 3000)))
