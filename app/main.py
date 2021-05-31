6e61e638212ee8cbea350478438c11acee9edecb
iam-mhaseeb <haseeb.emailbox@gmail.com>
2021-05-31 23:36:07 +0500

    Retired flask & used slack bolt

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
-from flask import Flask
+import os
 
-app = Flask(__name__)
+from dotenv import load_dotenv
+from slack_bolt import App
+
+load_dotenv()
 
 
-@app.route('/')
-def index():
-    return "<h1>Welcome to StandupMonkey</h1><br><p>A self hosted slack bot to conduct standups &amp; generate reports.</p>"
+def get_slack_bolt_app():
+    """
+    Returns slack bolt app object after initializing.
+    :return: Slack bolt app object
+    """
+    return App(
+        token=os.getenv("SLACK_BOT_TOKEN"),
+        signing_secret=os.getenv("SLACK_SIGNING_SECRET")
+    )
 
+
+# Start app
+if __name__ == "__main__":
+    app = get_slack_bolt_app()
+    app.start(port=int(os.environ.get("PORT", 3000)))
