58f93c7fc013f15f3ad1ac5d508f186739f86a6d
iam-mhaseeb <haseeb.emailbox@gmail.com>
2021-06-06 19:49:45 +0500

    Added constraint to only send messages after standup status is complete

--- app/main.py
+++ app/main.py
@@ -1,11 +1,27 @@
 import os
+import logging
 
 from slack_bolt import App
+
+LOGGER = logging.getLogger(__name__)
 
 app = App(
     token=os.environ.get("SLACK_BOT_TOKEN"),
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
+
+STANDUP_STATUS = {
+    'yesterday': '',
+    'today': '',
+    'blocker': '',
+}
+
+
+def check_standup_status_submission_completed():
+    if STANDUP_STATUS['yesterday'] and STANDUP_STATUS['today'] and STANDUP_STATUS['blocker']:
+        return True
+
+    return False
 
 
 def ask_standup_status(say):
@@ -71,16 +87,20 @@ def repeat_text(ack, say, command):
 @app.action("lastday-action")
 def action_yesterday_standup_status(body, ack, say):
     ack()
-    say(f"<@{body['user']['id']}> submitted yesterday's standup status.")
+    LOGGER.info(body)
+    if check_standup_status_submission_completed():
+        say(f"<@{body['user']['id']}> submitted standup status.")
 
 
 @app.action("today-action")
 def action_today_standup_status(body, ack, say):
     ack()
-    say(f"<@{body['user']['id']}> submitted today's standup status.")
+    if check_standup_status_submission_completed():
+        say(f"<@{body['user']['id']}> submitted standup status.")
 
 
 @app.action("blocker-action")
 def action_blocker_standup_status(body, ack, say):
     ack()
-    say(f"<@{body['user']['id']}> submitted blocking status.")
+    if check_standup_status_submission_completed():
+        say(f"<@{body['user']['id']}> submitted standup status.")
