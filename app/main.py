6ec178688e87e0e90603a5643810bbc27b5a2e69
iam-mhaseeb <haseeb.emailbox@gmail.com>
2021-06-06 19:26:59 +0500

    Added standup command

--- app/main.py
+++ app/main.py
@@ -8,6 +8,75 @@ app = App(
 )
 
 
+def ask_standup_status(say):
+    say(
+        {
+            "blocks": [
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "dispatch_action": True,
+                    "type": "input",
+                    "element": {
+                        "type": "plain_text_input",
+                        "multiline": True,
+                        "action_id": "lastday-action"
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "What did you do yesterday?",
+                        "emoji": True
+                    }
+                },
+                {
+                    "dispatch_action": True,
+                    "type": "input",
+                    "element": {
+                        "type": "plain_text_input",
+                        "multiline": True,
+                        "action_id": "today-action"
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "What are you planning to do today?",
+                        "emoji": True
+                    }
+                },
+                {
+                    "dispatch_action": True,
+                    "type": "input",
+                    "element": {
+                        "type": "plain_text_input",
+                        "multiline": True,
+                        "action_id": "blocker-action"
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Anything blocking you?",
+                        "emoji": True
+                    }
+                }
+            ]
+        }
+    )
+
+
+@app.command("/standup")
+def repeat_text(ack, say, command):
+    ack()
+    ask_standup_status(say)
+
+
+# @app.action("button_click")
+# def action_button_click(body, ack, say):
+#     # Acknowledge the action
+#     ack()
+#     say(f"<@{body['user']['id']}> clicked the button")
+
 @app.message("hello")
 def message_hello(message, say):
     say(f"Hey there <@{message['user']}>!")
