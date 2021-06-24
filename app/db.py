5562ffb39975bff4f462715b1afa75b8ba543e22
iam-mhaseeb <haseeb.emailbox@gmail.com>
2021-06-24 21:11:43 +0500

    Updated upsert sql to handle onconflict clause

--- app/db.py
+++ app/db.py
@@ -61,15 +61,15 @@ def upsert_today_standup_status(user_id, channel=None, column_name=None, message
             modified_at
         )
         VALUES (?, ?, ?, ?)
-        ON CONFLICT(user_id, date) DO UPDATE SET 
-            yesterday=excluded.yesterday,
-            today=excluded.today,
-            blocker=excluded.blocker,
-            channel=excluded.channel
+        ON CONFLICT(user_id, date) DO UPDATE SET
+            {column_conflict_clause}
+            {channel_conflict_clause}
         ;
         """.format(
             column_name=f'{column_name},' if column_name else '',
-            channel=f'channel,' if channel else ''
+            channel=f'channel,' if channel else '',
+            column_conflict_clause=f'{column_name}=excluded.{column_name}' if column_name else '',
+            channel_conflict_clause=f'{channel}=excluded.{channel}' if channel else ''
         ),
         (user_id, today, channel, now) if channel else (user_id, today, message, now)
     )
